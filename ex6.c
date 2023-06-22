f1a2f70197c859a55f4b374d85481c583f722a5d
Beatriz Nascimento <131934165+beatrizgnascimento@users.noreply.github.com>
2023-06-22 11:32:31 -0300

    Create ex6.c

---
+++ ex6.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct carro
+{
+    char marca[40];
+    float preco;
+    int ano;
+}carro;
+
+int main()
+{
+    carro vetcarro[8];
+    float p;
+    int i;
+    
+    for(i = 0; i < 8; i++)
+    {
+        scanf(" %[^\n]", vetcarro[i].marca);
+        scanf("%f", &vetcarro[i].preco);
+        scanf("%d", &vetcarro[i].ano);
+    }
+    
+    do
+    {
+        scanf("%f", &p);
+         if (p > 0)
+    {
+        printf("Carros mais baratos que %.2f:\n", p);
+        for (i = 0; i < 8; i++)
+        {
+            if (vetcarro[i].preco < p)
+            {
+                printf("%s %.2f %d\n", vetcarro[i].marca, vetcarro[i].preco, vetcarro[i].ano);
+                
+            }
+            
+        }
+        printf("\n");
+    }
+
+   } while (p > 0);
+    
+    
+    return 0;
+}
